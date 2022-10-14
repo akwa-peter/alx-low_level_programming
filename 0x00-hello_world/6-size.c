b957c552bf1d43f5ab4bbabd85e3a09c2b1bf2cd
akwa-peter <akwapeter1@gmail.com>
2022-10-14 14:27:53 -0700

    print size of all types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main entry part of the program
+ * return 0 - return integer to standard output
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longIntType;
+	long long int logLongIntType;
+	float floatType;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+	printf("Size of an int: %ld byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longIntType));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lonLongIntType));
+	printf("Size of a float: %ld byte(s)");
+
+	return (0);
+}
