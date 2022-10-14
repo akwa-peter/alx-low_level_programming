b0ba2b38455f5c758526e86ce547bae0a2e0d5c3
akwa-peter <akwapeter1@gmail.com>
2022-10-14 15:07:34 -0700

    print to standard output using scanf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - main entry of the program
+ * return 1 - return valueof main
+ */
+int main(void)
+{
+	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
