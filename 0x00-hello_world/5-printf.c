217e402c31c4155924e8862f51ab75eaf2939cf2
akwa-peter <akwapeter1@gmail.com>
2022-10-14 14:11:39 -0700

    Create c program using printf method

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - the starting point of a c program
+ * return - return integer value 0 aon main
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
