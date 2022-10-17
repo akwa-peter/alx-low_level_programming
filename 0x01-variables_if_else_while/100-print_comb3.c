fea08785d9a9f5ab0b4e90c66101320e5d7f015c
akwa-peter <akwapeter1@gmail.com>
2022-10-17 14:49:51 -0700

    Print possible combination of 2 digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,31 +6,22 @@
  */
 int main(void)
 {
-	int num = 0;
-
-	while (num < 10)
+	for (int num1 = 0; num1 < 10; num1++)
 	{
-		int num2 = num + 1;
-
-		while (num2 < 10)
+		for (int num2 = num1 + 1; num2 < 10; num2++)
 		{
-			putchar((num % 10) + '0');
+			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
 
-			if (num == 8 && num2 == 9)
+			if (num1 == 8 && num2 == 9)
 				continue;
 
 			putchar(',');
 			putchar(' ');
-
-			num2++;
 		}
-
-		num++;
 	}
 
 	putchar('\n');
 
 	return (0);
 }
-
