793508325cbb1b4bca693a3011868afbe83ca411
akwa-peter <akwapeter1@gmail.com>
2022-10-17 13:13:09 -0700

    Print all numbers in base 16

---
+++ 0x01-variables_if_else_while/8-print_base_16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - main program point to print base 16 numbers
+ * Return: return 0 always success
+ */
+int main(void)
+{
+	int num = 0;
+	char ch = 'a';
+
+	while (num < 10)
+	{
+		putchar((num % 10) + '0');
+		num++;
+	}
+
+	while (ch <= f)
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
