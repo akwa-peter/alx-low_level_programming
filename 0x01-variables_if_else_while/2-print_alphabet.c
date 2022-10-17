edc9fcb2fd66955259f7fb6e15af6c4d3e9a5406
akwa-peter <akwapeter1@gmail.com>
2022-10-17 03:21:20 -0700

    Print lower case of english alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main is the entry point of this program
+ * return 0 - 0 is the return value of this program
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch < 'z')
+	{
+		putchar(ch);
+		printf("\n");
+	}
+
+	return (0);
+}
