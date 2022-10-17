a6955d8f87c422164f1e56cdabe3c812c50b19f8
akwa-peter <akwapeter1@gmail.com>
2022-10-17 07:56:06 -0700

    Print lowercase and upper case letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main entry of the program
+ * Return: return 0 always success
+ */
+int main(void)
+{
+	char ch = a;
+	char CH = A;
+
+	while (ch <= 'z')
+	{
+		printChar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		printChar(CH);
+		CH++;
+	}
+
+	printf("\n");
+
+}
