c701071f7dbb89414b1c668ec572b9af0e6b9211
akwa-peter <akwapeter1@gmail.com>
2022-10-18 17:19:33 -0700

    create method to print alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char ch;
+
+	while (i < 10)
+	{
+		ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
