836b1b557e8a2ac41dacde57a92682b2b33cee2b
akwa-peter <akwapeter1@gmail.com>
2022-10-19 00:02:03 -0700

    Check for lowercase and uppercase letter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * islower - this checks for lower case letter
+ * ch - ch is the character argument to be checked
+ * Return: 1 if islower, or 0 if not
+ */
+int islower(int ch)
+{
+	if (_islower(ch))
+		return (1);
+	else
+		return (0);
+}
