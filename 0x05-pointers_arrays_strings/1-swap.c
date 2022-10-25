de80745ad4b01c413abfe573471c2c07a25218c2
akwa-peter <akwapeter1@gmail.com>
2022-10-25 03:41:35 -0700

    swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of th variables passed
+ * @a: variable 1
+ * @b: variable 2
+ */
+void swap_int(int *a, int *b)
+{
+	int *copy = *a;
+	*a = *b;
+	*b = *copy;
+}
