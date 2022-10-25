b10e6a7c20b873d4ec632ea9b845e886ca85637e
akwa-peter <akwapeter1@gmail.com>
2022-10-25 04:44:14 -0700

    print string in reverse order

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,11 @@
 void print_rev(char *s)
 {
 	int len = strlen(s);
-	char c[len] = s;
 
-	for (int i = len-1; i >= 0; i--)
+	int i;
+	for (i = len-1; i >= 0; i--)
 	{
-		_putchar(c[i]);
+		_putchar(s[i]);
 	}
 	_putchar("\n");
 
