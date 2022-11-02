831e4dd7b242a4ee53cb53786a2aead04e5ec156
akwa-peter <akwapeter1@gmail.com>
2022-11-02 07:40:52 -0700

    Return first occurance of subsring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (i = 0; needle[i]; i++)
 		{
-			if (haystck[i] != needle[i])
+			if (haystack[i] != needle[i])
 				break;
 		}
 		if (i != s)
