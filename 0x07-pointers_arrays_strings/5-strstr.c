58a308792191bd6e20c121a09e2bb2953c307bf5
akwa-peter <akwapeter1@gmail.com>
2022-11-02 07:42:10 -0700

    Return first occurance of subsring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,15 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+* _strstr - locates a substring
+*
+* @haystack: the longer string to search
+* @needle: the substring to search for
+*
+* Return: a pointer to the beginning of the located substring, or NULL if
+* the substring is not found.
+*/
 char *_strstr(char *haystack, char *needle)
 {
 	int i;
