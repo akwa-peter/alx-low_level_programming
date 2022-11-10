dd33ddd05342a72b26a52823c4039b54cdbf16f6
akwa-peter <akwapeter1@gmail.com>
2022-11-10 06:22:41 -0800

    write a function to print string on screen

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print the string on screen
+ * @s: string to be printed
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s[0] != '\n')
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+}
